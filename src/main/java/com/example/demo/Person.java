6996b4d15dfb2938fa821e04bea3ec2c63feb79a
g.cze <gabocze@gmail.com>
2020-09-24 13:51:15 -0300

    Mapping JPA.

--- src/main/java/com/example/demo/Person.java
+++ src/main/java/com/example/demo/Person.java
@@ -1,21 +1,42 @@
 package com.example.demo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @Entity
+@RestResource
+@Table(name="PERSON")
 public class Person {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name="ID")
   private long id;
 
+  @Column(name="FIRSTNAME")
   private String firstName;
+  @Column(name="LASTNAME")
   private String lastName;
+  @Column(name="CUIL")
   private String CUIL;
+	public Person()
+	{
+		super();
+	}
 
+	public Person(Integer id, String firstname, String lastname)
+	{
+		super();
+		this.id = id;
+		this.firstName = firstname;
+		this.lastName = lastname;
+	}
   public String getFirstName() {
     return firstName;
   }
