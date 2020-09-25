388f33f60867f4bb1930aa28df9ad0d12ebc3366
g.cze <gabocze@gmail.com>
2020-09-25 08:47:01 -0300

    Setting a miscellaneous up arround H2 connectivity and Data model.

---
+++ src/main/resources/_data.sql
@@ -0,0 +1,8 @@
+--insert into todo
+--values(1, 'Learn more Spring Boot');
+
+--insert into todo
+--values(2, 'Go buy milk');
+
+insert into person
+values(1,1234,'Luis','Robles');
---
+++ src/main/resources/_schema.sql
@@ -0,0 +1,5 @@
+create table todo (
+    id integer not null,
+    title varchar(255) not null,
+    primary key(id)
+);
